78c53575fb19133db937bdb6152016c17e4a7b98
ayush <ayushjaswal4543@gmail.com>
2023-02-26 21:35:28 +0530

    Added solution

---
+++ Arrays/RemoveDuplicatesfromSortedArray.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int l = 1, r;
+        for (r = 1; r < nums.size(); r++)
+        {
+            if (nums[r] != nums[r - 1])
+            {
+                nums[l] = nums[r];
+                l++;
+            }
+        }
+        return l;
+    }
+};
